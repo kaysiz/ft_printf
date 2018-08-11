528e1805b7dc68c1614ce9ecb96fae83da24a149
kaysiz <ksiziva@gmail.com>
2018-08-11 09:44:05 +0200

    modified that

--- ft_printf.c
+++ ft_printf.c
@@ -6,8 +6,17 @@
 /*   By: ksiziva <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/04 17:35:18 by ksiziva           #+#    #+#             */
-/*   Updated: 2018/08/04 17:35:20 by ksiziva          ###   ########.fr       */
+/*   Updated: 2018/08/05 09:19:13 by ksiziva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "print.h"
 
+int ft_printf(const char *format, ...)
+{
+	int i;
+
+	i = 0;
+
+	return(i);
+}
--- printf.h
+++ printf.h
@@ -1,8 +1,11 @@
 #ifndef PRINTF_H
 # define PRINTF_H
+
 # include <stdlib.h>
 # include <unistd.h>
+# include "includes/libft.h"
 # include <string.h>
+# include <stdarg.h>
 
 void	print_hex_l(int n);
 void	print_hex_u(int n);
